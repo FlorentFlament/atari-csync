1c7d61c479ddb7d03712f4476736336c93b751bc
Florent Flament <contact@florentflament.com>
2023-04-17 09:17:33 +0200

    [csync-pulse] shift output csync signal to be able to read vsync value

--- csync-pulse.sv
+++ csync-pulse.sv
@@ -1,7 +1,11 @@
 // Generates clean csync pulse when seeing hsync pulse
 // vsync is used when no pulse is being sent
+//
+// a pulse is --__ for a total of 10us (i.e 120 ticks at 12MHz)
+// plus we need 3us i.e 36 ticks before to be late enough to know vsync level.
+// nearest power of 2 are 64 ticks wait + 64 ticks up + 64 ticks down
 module csync_pulse
-  #(parameter CNT_WIDTH = 8)
+  #(parameter CNT_WIDTH = 7)
    (
     input  clk,
     input  hsync,
@@ -22,7 +26,7 @@ module csync_pulse
       hreg    = 2'b11;
       vreg    = 2'b11;
       creg    = 1;
-      counter = 2**(CNT_WIDTH-1);
+      counter = 192; // i.e end of cycle
    end
 
    always @(posedge clk)
@@ -34,16 +38,18 @@ module csync_pulse
 
         if (hpulse)
 	  counter <= 0;
-	else if (counter[CNT_WIDTH-1] == 0)
+	else if (counter < 192) // 192 ticks pulse cycle
 	  counter <= counter + 1;
 	else
 	  counter <= counter;
 
-	if (counter[CNT_WIDTH-1] == 1)
+	if (counter < 64)
 	  creg <= vreg[1];
-	else if (counter[CNT_WIDTH-2] == 0)
+	else if (counter < 128)
 	  creg <= 1;
-	else
+	else if (counter < 192)
 	  creg <= 0;
+	else
+	  creg <= vreg[1];
      end // always @ (posedge clk)
 endmodule
